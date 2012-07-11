41851726216112684785ab346bb31bacaa0cfbd9
Alan van Dam <alan@vandamnet.nl>
2012-07-11 12:10:37 +0200

    Show responses with websockets

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,38 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.libs.ws.WS
-import com.ning.http.client.Realm.AuthScheme
-import scala.xml.Elem
-import java.util.UUID
-import play.api.libs.iteratee.Enumerator
-import play.api.data.Form
-import play.api.data.Forms._
-import models.Reservation
 import java.util.Date
+import java.util.UUID
+
+import scala.annotation.implicitNotFound
+import scala.xml.Elem
+import scala.xml.Node
+import scala.xml.NodeSeq
+import scala.xml.PrettyPrinter
+
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+
+import com.ning.http.client.Realm.AuthScheme
+
+import models.Reservation
+import play.api.data.Form
+import play.api.data.Forms.date
+import play.api.data.Forms.mapping
+import play.api.data.Forms.nonEmptyText
+import play.api.data.Forms.text
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.iteratee.PushEnumerator
+import play.api.libs.ws.WS
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.mvc.WebSocket
 
 object Application extends Controller {
 
   val providerUrl = "http://localhost:8082/bod/nsi/v1_sc/provider"
   val credentials = "nsi" -> "nsi123"
+  val dateTimeFormat = ISODateTimeFormat.dateTime()
 
   val form: Form[Reservation] = Form(
       mapping(
@@ -34,7 +51,7 @@ object Application extends Controller {
 
   def requestForm = Action {
     val defaultForm = form.fill(
-        Reservation(description = "test", startDate = new Date, endDate = new Date, connectionId = generateConnectionId)
+        Reservation(description = "test", startDate = new Date, endDate = new Date, connectionId = generateConnectionId, source = "First port", destination = "Second port")
     )
 
     Ok(views.html.request(defaultForm))
@@ -45,24 +62,49 @@ object Application extends Controller {
         formWithErrors => BadRequest(views.html.request(formWithErrors)),
         reservation => Async {
           val reserveRequest = reserve(reservation)
-          println(reserveRequest)
           WS.url(providerUrl)
             .withAuth(credentials._1, credentials._2, AuthScheme.BASIC)
             .post(reserveRequest).map { response =>
-              Ok(response.xml)
+              Ok(views.html.response(prettify(response.xml)))
           }
         }
     )
   }
 
-  def reply = Action {
+  private def prettify(xml: Node): String = {
+    val printer = new PrettyPrinter(80, 4)
+    printer.format(xml)
+  }
+
+  private def prettify(xml: NodeSeq): String = {
+    xml.foldLeft("")((a, b) => a + prettify(b))
+  }
+
+  var clients: List[PushEnumerator[String]] = List()
+
+  def reply = Action { request =>
+    clients.foreach { client =>
+      client.push(prettify(request.body.asXml.get))
+    }
     Ok
+  }
+
+  def wsRequest = WebSocket.using[String] { request =>
+    val in = Iteratee.foreach[String](println).mapDone { _ =>
+      println("Disconnected")
+    }
+
+    lazy val out: PushEnumerator[String] = Enumerator.imperative[String]()
+
+    clients = out :: clients
+
+    (in, out)
   }
 
   private def generateConnectionId = "urn:uuid:%s".formatted(UUID.randomUUID.toString)
 
   private def reserve(reservation: Reservation) = {
-    val replyTo = routes.Application.reply
+    val replyTo = "http://localhost:9000" + routes.Application.reply
     putInEnveloppe(
       <int:reserveRequest>
         <int:correlationId>urn:uuid:correlationId</int:correlationId>
@@ -75,8 +117,8 @@ object Application extends Controller {
             <description>{ reservation.description }</description>
             <serviceParameters>
               <schedule>
-                <startTime>{ reservation.startDate }</startTime>
-                <endTime>{ reservation.endDate }</endTime>
+                <startTime>{ dateTimeFormat.print(new DateTime(reservation.startDate)) }</startTime>
+                <endTime>{ dateTimeFormat.print(new DateTime(reservation.endDate)) }</endTime>
               </schedule>
               <bandwidth>
                 <desired>1000</desired>
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+        "joda-time" % "joda-time" % "2.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
     )
 
 }
