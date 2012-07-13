319d21dc903f5021680b188a1d39de89ffe4179d
Alan van Dam <alan@vandamnet.nl>
2012-07-13 18:35:56 +0200

    moved duplicate provider fields to a fragment

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,21 @@
 $(function() {
-  window.prettyPrint && prettyPrint()
+
+  var initExtraFields = function() {
+      var extraFields = $("fieldset.extra-fields");
+
+      if (extraFields.length) {
+
+          $(".message.extra-fields").on('click', function(event) {
+              extraFields.toggle();
+              $(this).toggleClass('expanded');
+          });
+
+          extraFields.toggle();
+      }
+  };
+
+  initExtraFields();
+
+  window.prettyPrint && prettyPrint();
+
 })
