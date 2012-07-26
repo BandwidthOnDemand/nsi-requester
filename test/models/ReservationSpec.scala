b0a17403dea1cf0f9f852ad8594579a8b99d8516
Alan van Dam <alan@vandamnet.nl>
2012-07-26 15:32:05 +0200

    reduce code duplication for ReservationSpec

--- test/models/ReservationSpec.scala
+++ test/models/ReservationSpec.scala
@@ -12,36 +12,39 @@ class ReservationSpec extends Specification {
 
     "have an envelope with endTime" in {
       val endDate = new DateTime(2015, 1, 1, 12, 10).toDate
-      val res = Reservation(None, new Date, Left(endDate), "con", "source", "dest", 10, "cor", "http://localhost", "nsa:surfnet.nl")
+      val res = defaultReservation(end = Left(endDate))
 
       res.toEnvelope must \\("endTime") \> "2015-01-01T12:10:00.000+01:00"
     }
 
     "have an envelope with duration" in {
-      val res = Reservation(None, new Date, Right(new Period(2, 10, 0, 0)), "con", "source", "dest", 10, "corr", "http://localhost",
-        "nsa:surfnet.nl")
+      val res = defaultReservation(end = Right(new Period(2, 10, 0, 0)))
 
       res.toEnvelope must \\("duration") \> "PT2H10M"
     }
 
     "have an envelope with a description" in {
-      val res = Reservation(Some("My new reservation"), new Date, Left(new Date), "con", "source", "dest", 10, "cor", "http://localhost", "nsa:surfnet.nl")
+      val res = defaultReservation(description = Some("My new reservation"))
 
       res.toEnvelope must \\("description") \> "My new reservation"
     }
 
     "have an envelope without a description" in {
-      val res = Reservation(None, new Date, Left(new Date), "con", "source", "dest", 10, "cor", "http://localhost", "nsa:surfnet.nl")
+      val res = defaultReservation(description = None)
 
       res.toEnvelope must not \\("description")
     }
 
     "have an envelope with a globalReservationId" in {
-      val res = Reservation(None, new Date, Left(new Date), "con", "source", "dest", 10, "cor", "http://localhost", "nsa:surfnet.nl",
-        Some("urn:surfnet:123456"))
+      val res = defaultReservation(globalReservationId = Some("urn:surfnet:123456"))
 
       res.toEnvelope must \\("globalReservationId") \> "urn:surfnet:123456"
     }
+  }
+
+  object defaultReservation {
+    def apply(description: Option[String] = None, end: Either[Date, Period] = Left(new Date()), globalReservationId: Option[String] = None) =
+      Reservation(description, new Date(), end, "connection", "source", "dest", 10, "correlation:1", "http://localhost", "nsa:surfnet.nl", globalReservationId)
   }
 
 }
