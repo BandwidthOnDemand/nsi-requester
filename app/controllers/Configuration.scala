6675d0cd84082312fe4d0a68d6803dd0ef98b4fb
Alan van Dam <alan@vandamnet.nl>
2014-02-27 14:59:07 +0100

    Requester NSA id is configurable in application.conf

--- app/controllers/Configuration.scala
+++ app/controllers/Configuration.scala
@@ -1,17 +1,17 @@
 package controllers
-
 import models._
 import play.api.mvc.Request
 import play.api.mvc.AnyContent
+import play.api.Play.current
 import scala.util.Try
 import java.net.URI
 
-object Defaults {
+object Configuration {
 
   val DefaultPortPrefix = "urn:ogf:network:surfnet.nl:1990:testbed:"
   val DefaultProvider = Provider.all.head
   val DefaultServiceType = "http://services.ogf.org/nsi/2013/12/descriptions/EVTS.A-GOLE"
-  val RequesterNsa = "urn:ogf:network:nsa:surfnet-nsi-requester"
+  val RequesterNsa = current.configuration.getString("application.nsi.requesterNsa").getOrElse(sys.error("Requester NSA is not configured (application.nsi.requesterNsa)"))
 
   def currentEndPoint(implicit request: Request[AnyContent]) = {
     val provider = request.session.get("nsaId").flatMap(id => Provider.find(id)).getOrElse(DefaultProvider)
