7a7d10461e9d5bd3893e53e32bc7c0032bd40bbf
Alan van Dam <alan@vandamnet.nl>
2014-04-14 11:42:47 +0200

    Show the version of the requester in settings view

--- app/controllers/SettingsController.scala
+++ app/controllers/SettingsController.scala
@@ -5,18 +5,19 @@ import play.api.data.Form
 import play.api.data.Forms._
 import models._
 import RequesterSession._
+import support.BuildInfo
 
 object SettingsController extends Controller {
 
   def settingsForm = Action { implicit request =>
     val defaultForm = settingsF.fill(currentEndPoint)
 
-    Ok(views.html.settings(defaultForm))
+    Ok(views.html.settings(defaultForm, VersionString))
   }
 
   def settings = Action { implicit request =>
     settingsF.bindFromRequest.fold[SimpleResult](
-      formWithErrors => BadRequest(views.html.settings(formWithErrors)),
+      formWithErrors => BadRequest(views.html.settings(formWithErrors, VersionString)),
       {
         case endPoint =>{
           Redirect(routes.Application.reserveForm)
@@ -24,10 +25,11 @@ object SettingsController extends Controller {
             .withSession(
               AccessTokensSessionField -> endPoint.accessTokens.mkString(","),
               ProviderNsaSessionField -> endPoint.provider.nsaId)
-
         }
       })
   }
+
+  private lazy val VersionString = s"${BuildInfo.version} (${BuildInfo.gitHeadCommitSha})"
 
   private[controllers] val settingsF: Form[EndPoint] = Form(
     mapping(
--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import sbtbuildinfo.Plugin._
 
 object ApplicationBuild extends Build {
 
@@ -11,9 +12,19 @@ object ApplicationBuild extends Build {
     "joda-time" % "joda-time" % "2.1"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  lazy val gitHeadCommitSha = settingKey[String]("current git commit SHA")
+
+  val main = play.Project(appName, appVersion, appDependencies,
+    settings = play.Project.playScalaSettings ++ buildInfoSettings
+  ).settings(
+    organization := "nl.surfnet.bod",
     scalaVersion := "2.10.4",
-    scalacOptions := Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
+    scalacOptions := Seq("-deprecation", "-feature", "-unchecked", "-Xlint"),
+
+    gitHeadCommitSha := Process("git rev-parse --short HEAD").lines.head,
+    sourceGenerators in Compile <+= buildInfo,
+    buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, gitHeadCommitSha),
+    buildInfoPackage := "support"
   )
 
 }
